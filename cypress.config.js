75dae21d767b16a8f494cb89ddcac0aa670874e7
up6dn10 <harry.song@foxmail.com>
2025-10-02 20:09:26 +0800

    ci(allure): deploy Allure report to gh-pages and comment PR

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,16 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // register allure plugin
+      try {
+        const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+        allureWriter(on, config);
+      } catch (err) {
+        // plugin might not be installed in local env; CI will install
+        // eslint-disable-next-line no-console
+        console.warn('Allure plugin not registered:', err && err.message);
+      }
+      return config;
     },
     env: {
       managerPortalUrl: 'http://localhost:8002',
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,4 +1,5 @@
 import './commands'
+import '@shelex/cypress-allure-plugin';
 
 Cypress.on('uncaught:exception', (err) => {
   if (err.message.includes('Script error.') || err.message.includes('cross origin')) {
